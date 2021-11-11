c46907bccddc78ab2ad587590dd9928a31f3874f
Marcus Greasly <marcus.greasly@avanade.com>
2021-11-11 14:54:13 +1000

    Moved default (state implementation) into folder

---
+++ src/Container.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import State from "././state/App";
+
+export default () => {
+    return (
+        <>
+            <h1>using State</h1>
+            <State />
+        </>
+    )
+}
